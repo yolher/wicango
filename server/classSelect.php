17e006fa942c4b567a859b18169ca79ac537955a
oaguillondev <aguillon_@hotmail.com>
2014-01-15 22:15:26 -0500

    add new searcher random

--- server/classSelect.php
+++ server/classSelect.php
@@ -7,7 +7,39 @@ class select{
 	public function getSearch($palabra,$urlRequest){
 	
 		$SearchAlert = array('error' => 'Su busqueda no produjo ningun resultado');//alerta del sistema, guarda un array para convertir en objeto json
-		$sql = "select id_entry,name_entry,direccion_entry,telefono_entry,correo_entry,celular_entry from app_entry where name_entry like '%".$palabra."%' || palabras_clave like '%".$palabra."%'";//guarda una consulta o sentencia sql
+		//$sql = "select id_entry,name_entry,direccion_entry,telefono_entry,correo_entry,celular_entry from app_entry where name_entry like '%".$palabra."%' || palabras_clave like '%".$palabra."%'";//guarda una consulta o sentencia sql
+
+		$filtro = str_replace(" ", ",", $palabra);
+		$numPalabra = str_word_count($palabra);
+
+		$arrayPa = explode(",",$filtro);
+		
+		$nameFile = $arrayPa[0].rand(0,9999);
+
+		$cabecera = "select id_entry,name_entry,direccion_entry,telefono_entry,correo_entry,celular_entry from app_entry where name_entry like '%".$palabra."%' || palabras_clave like '%".$arrayPa[0]."%' ";
+		//========================================================================================
+
+		if ($numPalabra > 1) {
+			//========================================================================================
+			for($i = 1; $i < $numPalabra; $i++){
+				$bodyConsul = " and palabras_clave like '%".$arrayPa[$i]."%'";
+				$fileW=fopen("".$nameFile.".txt","a") or die("Problemas");	  	
+				fputs($fileW,$bodyConsul);
+				fclose($fileW);
+			}
+			//========================================================================================
+			
+
+			$fileR = fopen("".$nameFile.".txt", "r") or exit("Unable to open file!");
+			$sql = $cabecera.fgets($fileR);
+			fclose($fileR);
+			//========================================================================================
+			unlink("".$nameFile.".txt");
+			// ======================================================================================
+		}else{
+			$sql = $cabecera;
+		}
+
 		//guarda el objeto de conecion con la base de datos
 		$enlace = Conectar::con(); 
 
